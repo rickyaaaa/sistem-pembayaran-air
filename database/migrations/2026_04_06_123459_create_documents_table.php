d7cf592f65dcf050492fa5c47b3fcf9952dcafd9
unknown <ricky@gmail.com>
2026-04-07 12:24:54 +0700

    menambahkan composer

--- database/migrations/2026_04_06_123459_create_documents_table.php
+++ database/migrations/2026_04_06_123459_create_documents_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('file_path');
-            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('created_by')->nullable();
             $table->timestamps();
 
             $table->foreign('created_by')->references('id')->on('users')->nullOnDelete();
